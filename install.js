13f4681c66d7ea74d69eae38836e6b35f66e3842
William Molina <wdmolina@bancolombia.com.co>
2022-04-21 19:37:33 -0500

    Modificar la estrategia de actualizacion de recursos

--- install.js
+++ install.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        registration.update();
+        //registration.update();
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
--- sw.js
+++ sw.js
@@ -21,21 +21,39 @@ self.addEventListener('install', function(event) {
     );
 });
 
-
+/* Network first */
 function networkFirst(event){
     event.respondWith(
         fetch(event.request).then(function(response){
-        return caches.open('main').then(function(cache){
-            cache.put(event.request, response.clone());
-            return response;
-        })
+          return caches.open('main').then(function(cache){
+              cache.put(event.request, response.clone());
+              return response;
+          })
         }).catch(function() {
             return caches.match(event.request);
         })
     )
 }
+/* Cache first */
+function cacheFirst(event){
+  event.respondWith(
+    caches.open('main').then(function(cache) {
+      return cache.match(event.request).then(function(cacheResponse) {
+        if(cacheResponse)
+          return cacheResponse
+        else
+          return fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone())
+              return networkResponse
+          })
+      })
+    })
+  );
+}
 
-/* Network first */
 self.addEventListener('fetch', function (event) {
-    return networkFirst(event);
+    if(event.request.url.includes('resources'))
+      return cacheFirst(event);
+    else
+      return networkFirst(event);
 });
